98b65782b2d726b5d4641a51aac3d3908a8d3a60
parth-nikam <parthsnikam12co@gmail.com>
2023-03-23 21:10:02 +0530

    Bug Fix. Resign First Responder while deselecting the search bar

--- ToDoooo/Controllers/ToDoListViewController.swift
+++ ToDoooo/Controllers/ToDoListViewController.swift
@@ -99,14 +99,15 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(request: NSFetchRequest<Item> = Item.fetchRequest()){
-        let request: NSFetchRequest <Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+       
+    
         do{
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
-        
+        tableView.reloadData()
     }
 }
 
@@ -115,14 +116,26 @@ class ToDoListViewController: UITableViewController {
 
 extension ToDoListViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         
         request.predicate = NSPredicate(format: "title Contains[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
     
-        loadItems(request: request)
+        loadItems(with: request)
         
-        tableView.reloadData()
+
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
 }
