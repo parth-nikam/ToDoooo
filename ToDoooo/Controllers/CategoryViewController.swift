a96ce3b07d3357e065908db9380f98ceb86d7441
parth-nikam <parthsnikam12co@gmail.com>
2023-03-24 17:52:06 +0530

    Fetching Data from Realm.

--- ToDoooo/Controllers/CategoryViewController.swift
+++ ToDoooo/Controllers/CategoryViewController.swift
@@ -12,9 +12,7 @@ class CategoryViewController: UITableViewController {
     
     let realm = try! Realm()
 
-    var categories = [`Category`]()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var categories: Results<Category>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +24,14 @@ class CategoryViewController: UITableViewController {
     //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        cell.textLabel?.text = categories[indexPath.row].name
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
         
         return cell
     }
@@ -48,7 +46,7 @@ class CategoryViewController: UITableViewController {
         let destinationVC = segue.destination as! ToDoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow{
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
     }
 
@@ -68,14 +66,10 @@ class CategoryViewController: UITableViewController {
     }
     
     func loadCategories(){
-//        let request: NSFetchRequest<Category> = Category.fetchRequest()
-//        
-//        do{
-//            categories = try context.fetch(request)
-//        } catch {
-//            print("Error loding categories \(error)")
-//        }
-//        tableView.reloadData()
+        
+        categories = realm.objects(Category.self)
+        
+        tableView.reloadData()
     }
     
     //MARK: - Add New Categories
@@ -91,7 +85,6 @@ class CategoryViewController: UITableViewController {
             let newCategory = Category()
             newCategory.name = textField.text!
             
-            self.categories.append(newCategory)
             
             self.save(category: newCategory)
             
